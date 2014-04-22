191adc1ddc037260e5cfffd5c1e9af3a1072d554
rachellogie <rachel.logie1@gmail.com>
2014-04-22 14:42:36 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FruitsApp::Application.config.secret_key_base = 'f6d1bc7bdd5496eb3d194faae6e965bce7fa8ad5202f4505d8e7f7892c4f08677fde86adabf8a17bd2af0b718ebabd65fa3c48de586321b8f0dbed888c4afe4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FruitsApp::Application.config.session_store :cookie_store, key: '_fruits_app_session'
